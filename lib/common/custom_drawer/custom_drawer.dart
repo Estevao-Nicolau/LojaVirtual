393dd330fb0d7b2d0b0171fe3f3cecd75a10e4d4
nicolau <estevao_nicolau@hotmail.com>
2022-07-22 16:53:05 -0300

    :building_construction:

---
+++ lib/common/custom_drawer/custom_drawer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lojavirtual/common/custom_drawer/drawer_tile.dart';
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: const <Widget>[
+          DrawerTile(iconData: Icons.home, title: 'Inicio', page: 1),
+          DrawerTile(iconData: Icons.list, title: 'Produtos',page: 2),
+          DrawerTile(iconData: Icons.playlist_add_check, title: 'Meus Pedidos', page: 3,),
+          DrawerTile(iconData: Icons.location_on, title: 'Loja', page: 4),
+        ],
+      ),
+    );
+  }
+}
