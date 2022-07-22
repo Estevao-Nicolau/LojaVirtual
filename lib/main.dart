ca9289177970dda80af694cc146836cc7c370afe
nicolau <estevao_nicolau@hotmail.com>
2022-07-22 00:13:18 -0300

    :wrench:firestore-test

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_dynamic_calls
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
+
+  FirebaseFirestore.instance
+      .collection('pedidos')
+      .doc('#00001')
+      .set({'preco': 100.00, 'usuario': 'nicolau brito'});
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
